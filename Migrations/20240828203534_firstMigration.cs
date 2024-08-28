8773f475c292b9bfb30b3ad575018e1264d72619
Thallys Yon Souza Feitosa <thallysyon@hotmail.com>
2024-08-28 17:51:07 -0300

    Finalização dos testes

---
+++ Migrations/20240828203534_firstMigration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstruturaDoProduto.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    Codigo = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: true),
+                    CodigoProdutoFornecedor = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: true),
+                    DataDeCadastro = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CustoPadrao = table.Column<decimal>(type: "decimal(8,4)", nullable: false),
+                    PrecoFinal = table.Column<decimal>(type: "decimal(8,4)", nullable: false),
+                    MargemDeLucroEmReais = table.Column<decimal>(type: "decimal(8,4)", nullable: false),
+                    PesoLiquido = table.Column<decimal>(type: "decimal(8,3)", nullable: true),
+                    PesoBruto = table.Column<decimal>(type: "decimal(8,3)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Produtos}/{action=Index}/{id?}");
 
 app.Run();
